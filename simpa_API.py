8f0d2df65de7ec44f9d5b461779d9dc699521515
TamerlanK <tamerlan2540@gmail.com>
2024-03-06 10:00:45 +0600

    Начало проекта добавил все в ГИТ

---
+++ simpa_API.py
@@ -0,0 +1,22 @@
+token1 = '7115967024:AAGyuN-ZwxJjjhus4R4oqpvaMDIBWc6Adew'
+
+#@dp.message(F.text == 'Информация')
+
+#@dp.message(Command("info"))
+#async def cmd_info(message: types.Message):
+    #await message.reply("Я бот - твой друг и товарищ ")
+
+#Хэндлер на команду /start
+#@dp.message(Command('start'))
+#async def start(message: types.Message):
+  #  name = message.chat.first_name
+   # await message.answer(F,'Привет, {name}')
+
+
+#async def main():
+    #await dp.start_polling(bot)
+
+#if __name__ == '__main__':
+ #  asyncio.run(main())
+
+
